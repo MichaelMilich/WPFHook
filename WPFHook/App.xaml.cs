ca36942ec9738574b6ccb2dd4c79ac4fc49a3bb7
Michael Millich <michael.milich1@gmail.com>
2021-04-19 07:01:02 +0300

    added code to handle unexpected exceptions. HAVE TO TEST. HAVE TO FIND A WAY TO TEST.
    future point is to handle the exceptions caught by the hook manager ( escalate it to main window and let it restart the middle man)

--- WPFHook/App.xaml.cs
+++ WPFHook/App.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +15,61 @@ namespace WPFHook
     /// </summary>
     public partial class App : Application
     {
+        private static int counter = 1;
+        public App() : base()
+        {
+            SetupUnhandledExceptionHandling();
+        }
+
+        private void SetupUnhandledExceptionHandling()
+        {
+            // Catch exceptions from all threads in the AppDomain.
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+                ShowUnhandledException(args.ExceptionObject as Exception, "AppDomain.CurrentDomain.UnhandledException", false);
+
+            // Catch exceptions from each AppDomain that uses a task scheduler for async operations.
+            TaskScheduler.UnobservedTaskException += (sender, args) =>
+                ShowUnhandledException(args.Exception, "TaskScheduler.UnobservedTaskException", false);
+
+            // Catch exceptions from a single specific UI dispatcher thread.
+            Dispatcher.UnhandledException += (sender, args) =>
+            {
+                // If we are debugging, let Visual Studio handle the exception and take us to the code that threw it.
+                if (!Debugger.IsAttached)
+                {
+                    args.Handled = true;
+                    ShowUnhandledException(args.Exception, "Dispatcher.UnhandledException", true);
+                }
+            };
+
+        }
+
+        void ShowUnhandledException(Exception e, string unhandledExceptionType, bool promptUserForShutdown)
+        {
+            App.LogExceptions(e, unhandledExceptionType);
+
+            var messageBoxTitle = $"Unexpected Error Occurred: {unhandledExceptionType}";
+            var messageBoxMessage = $"The following exception occurred:\n\n{e}";
+            var messageBoxButtons = MessageBoxButton.OK;
+
+            if (promptUserForShutdown)
+            {
+                messageBoxMessage += "\n\nNormally the app would die now. Should we let it die?";
+                messageBoxButtons = MessageBoxButton.YesNo;
+            }
+
+            // Let the user decide if the app should die or not (if applicable).
+            if (MessageBox.Show(messageBoxMessage, messageBoxTitle, messageBoxButtons) == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        public static async Task LogExceptions(Exception e, string unhandledExceptionType)
+        {
+            string s = DateTime.Now.ToString() + " Exception #" + counter + " :" + $"Unexpected Error Occurred: {unhandledExceptionType} " + $"The following exception occurred:\n\n{e}";
+            using StreamWriter file = new StreamWriter("ExceptionLog.txt", append: true);
+            await file.WriteLineAsync(s);
+        }
     }
 }
