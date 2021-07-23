7da0af3ea4cbd5f9549651379bb62e9929095be6
DESKTOP-9AM932K\User <michael.milich1@gmail.com>
2021-07-23 22:18:44 +0300

    made the tag window fill itself up. It seems that the code works but the editor of visual studio tells me something is off... ?I dont know what.

---
+++ WPFHook/Commands/TagsSizeConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPFHook.Commands
+{
+    class TagsSizeConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double actualHeight = (double)values[0];
+            int count = (int)values[1]+1;
+            double rval = actualHeight / count;
+
+            if (targetType == typeof(Thickness))
+            {
+                return new Thickness(rval, 0, 0, 0);
+            }
+            else
+            {
+                return rval;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
